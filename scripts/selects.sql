d182c6288201fbab36cc8b3b6eb12e235c1abc6d
bruno.pfohl <bruno.pfohl@seznam.cz>
2021-01-07 20:17:02 +0100

    Selecty upraveny
    Selecty upraveny tak, aby dotahovali VŠECHNY potřebná data pro aplikaci.

--- scripts/selects.sql
+++ scripts/selects.sql
@@ -1,15 +1,15 @@
 -- 1) Select in select - Pacienti a počet jejich rezervací
-select Name, Surname, BookingCount = (select count(*) from Booking B where B.PacientId = P.Id) from Pacient P
+select P.*, [Count] = (select count(*) from Booking B where B.PacientId = P.Id) from Pacient P
 
 -- 2) Select ve from - Lidi co byly někdy na testu a vyšel pozitivní (tzn. lidi co prodělali covid)
-SELECT DISTINCT Name, PositiveGuys.Id
+SELECT DISTINCT Pacient.*
 FROM (SELECT Id
 	  FROM Booking
 	  WHERE IsPositive = 1) AS PositiveGuys, Pacient
 WHERE Pacient.Id = PositiveGuys.Id
 
 -- 3) Vnořený select ve where - všichni doktoři, kteří nemají pro daný den žádnou rezervaci.
-select Id, D.Name, D.Surname from Doctor D 
+select D.* from Doctor D 
 where Id not in (
 	select DTT.DoctorId from Booking B 
 	join DoctorToTest DTT on DTT.Id = B.DoctorToTestId
@@ -18,15 +18,15 @@ where Id not in (
 )
 
 -- 4) Group by - počet rezervací všech doktorů pro daný den, kteří mají alespoň jednu
-select DTT.DoctorId, count(DTT.DoctorId) from Booking B 
+select DTT.DoctorId, [Count] = count(DTT.DoctorId) from Booking B 
 join DoctorToTest DTT on DTT.Id = B.DoctorToTestId
 where B.StartsAt > '2020-12-29' and B.StartsAt < '2020-12-30'
 group by DTT.DoctorId
 
 -- 5) Množinová operace -> všechny místa v liberci a všechny místa, kde se provádí covid testy
-select L.Name, L.City, L.Street from Location L where City like 'Liberec' 
+select L.* from Location L where City like 'Liberec' 
 intersect
-select L.Name, L.City, L.Street from Location L where Id in (
+select L.* from Location L where Id in (
 	select D.LocationId from Test T 
 	join DoctorToTest DTT on DTT.TestId = T.Id
 	join Doctor D on DTT.DoctorId = D.Id 
@@ -35,6 +35,6 @@ select L.Name, L.City, L.Street from Location L where Id in (
 
 -- 6) left join - seznam všech testů a jací doktoři je provádí (pro počet by se mohl použít group by obdobně jako u bodu 4)
 -- Left join používám protože chci vidět i testy, které nikdo neposkytuje
-select T.Name as 'Název testu', D.Name, D.Surname from Test T 
+select T.Id, T.Name as 'TestName', T.Description, T.Price, T.Length, D.Name, D.* from Test T 
 left join DoctorToTest DTT on DTT.TestId = T.Id
 left join Doctor D on D.Id = DTT.DoctorId
